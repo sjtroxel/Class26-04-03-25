5c215a90c59069263715bd2a5804494c6951d67b
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-04-02 14:58:57 -0500

    intro side to more ng's

--- 02-starting-project-deepdive/src/app/dashboard/server-status/server-status.component.ts
+++ 02-starting-project-deepdive/src/app/dashboard/server-status/server-status.component.ts
@@ -15,6 +15,7 @@ export class ServerStatusComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log('ON INIT');
     setInterval(() => {
       const rnd = Math.random();      // 0 - 0.99999999999999999
 
@@ -26,5 +27,9 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = "unknown";
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
   }
 }
